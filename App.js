137ed54e54a78dfafddb1ddfa3ca71150b137181
Jeff Spacek <Jeffrey.spacek@gmail.com>
2024-04-15 22:03:00 -0700

    feat. build app.js using more react

--- App.js
+++ App.js
@@ -1,4 +1,14 @@
+// Import Screens
+import Start from "./components/Start";
+import Chat from "./components/Chat";
+
+// Import react Navigation
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
 import { useState } from "react";
+
+//delete later
 import {
   StyleSheet,
   View,
@@ -8,16 +18,8 @@ import {
   Alert,
   ScrollView,
 } from "react-native";
+//delete later
 
-// import the screens
-import Screen1 from "./components/Screen1";
-import Screen2 from "./components/Screen2";
-
-// import react Navigation
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
-// Create the navigator
 const Stack = createNativeStackNavigator();
 
 const App = () => {
@@ -29,50 +31,12 @@ const App = () => {
   };
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Screen1">
-        <Stack.Screen name="Screen1" component={Screen1} />
-        <Stack.Screen name="Screen2" component={Screen2} />
+      <Stack.Navigator initialRouteName="Start">
+        <Stack.Screen name="Start" component={Start} />
+        <Stack.Screen name="Chat" component={Chat} />
       </Stack.Navigator>
-      <View style={styles.container}>
-        <TextInput
-          style={styles.textInput}
-          value={text}
-          onChangeText={setText}
-          placeholder="Type Something Here"
-        />
-        <Text style={styles.textDisplay}>You wrote: {text}</Text>
-        <Button
-          onPress={() => {
-            alertMyText();
-          }}
-          title="Press Me"
-        />
-        <ScrollView>
-          <Text style={{ fontSize: 110 }}>
-            This text is so big! And so long! You have to scroll!
-          </Text>
-        </ScrollView>
-      </View>
     </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  textInput: {
-    width: "88%",
-    borderWidth: 1,
-    height: 50,
-    padding: 10,
-  },
-  textDisplay: {
-    height: 50,
-    lineHeight: 50,
-  },
-});
 
 export default App;
--- components/Start.js
+++ components/Start.js
@@ -1,39 +1,68 @@
-import { useState } from 'react';
-import { StyleSheet, View, Text, Button, TextInput } from 'react-native';
+import { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Button,
+  TextInput,
+  ImageBackground,
+  TouchableOpacity,
+} from "react-native";
 
-const Screen1 = ({ navigation }) => {
-  const [name, setName] = useState('');
+// Define the Start Component
+const Start = ({ navigation }) => {
+  // State to hold the name input value
+  const [name, setName] = useState("");
+  //State to hold the chosen background color
+  const [background, setBackground] = useState("");
+  const onPress = () => setCount((prevCount) => prevCount + 1);
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    textInput: {
+      width: "88%",
+      padding: 15,
+      borderWidth: 1,
+      marginTop: 15,
+      marginBottom: 15,
+      height: 50,
+    },
+    textDisplay: {
+      height: 50,
+      lineHeight: 50,
+    },
+    button: { alingItems: "center", 
+    backgroundColor: "#757083",
+    fontSize: "16",
+    fontWeight: "600",
+    fontColor: "#FFFFFF",
+    padding: 10 },
+  });
 
   return (
-    <View style={styles.container}>
-      <Text>Hello Screen1!</Text>
-      <TextInput
-        style={styles.textInput}
-        value={name}
-        onChangeText={setName}
-        placeholder='Type your username here'
-      />
-      <Button
-        title="Go to Screen 2"
-        onPress={() => navigation.navigate('Screen2', {name:name})}
-      />
-    </View>
+    <ImageBackground
+      source={require("../img/background_image.png")}
+      style={styles.imageBackground}
+      resizeMode="cover"
+    >
+      <View style={styles.appTitle}>
+        <Text>Welcome to Chat-App</Text>
+        <TextInput
+          style={styles.textInput}
+          value={name}
+          onChangeText={setName}
+          placeholder="Your Name"
+        />
+        <TouchableOpacity style={styles.button} onPress={onPress}>
+          <Text>Start Chatting</Text>
+        </TouchableOpacity>
+      </View>
+    </ImageBackground>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-     justifyContent: 'center',
-    alignItems: 'center'
-  },
-  textInput: {
-    width: "88%",
-    padding: 15,
-    borderWidth: 1,
-    marginTop: 15,
-    marginBottom: 15
-  }
-});
-
-export default Screen1;
+export default Start;
